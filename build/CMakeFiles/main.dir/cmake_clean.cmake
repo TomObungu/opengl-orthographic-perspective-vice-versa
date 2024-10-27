3b50c0ec21513f9257e206bd88d0b10beed23b7f
Tawitg <108408219+Tawitg@users.noreply.github.com>
2024-10-27 15:38:08 +0000

    Edited cmake

---
+++ build/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "C:/Users/deeza/Desktop/book_pdf_part_I/main.exe"
+  "C:/Users/deeza/Desktop/book_pdf_part_I/main.exe.manifest"
+  "C:/Users/deeza/Desktop/book_pdf_part_I/main.pdb"
+  "CMakeFiles/main.dir/src/main.cpp.obj"
+  "CMakeFiles/main.dir/src/main.cpp.obj.d"
+  "libmain.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
